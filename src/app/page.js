bfd7c44a9b28ad52bb35fe0f86ed4a47e89bd267
Bilal Raza <126255680+brmemon@users.noreply.github.com>
2024-01-06 16:20:42 +0500

    structure and error

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-// import "./style.scss"
 import Login from './auth/Login/page'
 import VerifyEmail from './auth/VerifyEmail'
 export default function Home() {
