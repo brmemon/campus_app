f25b113244284260e6fe0cbc007eb8f862561dd2
Bilal Raza <126255680+brmemon@users.noreply.github.com>
2024-01-08 17:38:13 +0500

    css problem 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Login from './auth/Login/page'
 import VerifyEmail from './auth/VerifyEmail'
+import { StyledEngineProvider } from '@mui/material/styles';
+
 export default function Home() {
   return (
     <>
-    <Login />
-    {/* <VerifyEmail /> */}
+      <StyledEngineProvider injectFirst>
+        <Login />
+      </StyledEngineProvider>
+      {/* <VerifyEmail /> */}
     </>
   )
 }
