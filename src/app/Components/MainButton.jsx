61a34059153f55f35296d4bb0f171e080f2105a1
Bilal Raza <126255680+brmemon@users.noreply.github.com>
2024-01-08 17:26:15 +0500

    css problem

--- src/app/Components/MainButton.jsx
+++ src/app/Components/MainButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Button } from "@mui/material";
-import styles from "../../../styles/scss/MainButton.module.scss";
+import "../../../styles/scss/MainButton.scss";
 
 const MainButton = ({ text, onClick, className, icon }) => {
   return (
     <Button
       onClick={onClick}
-      className={`${styles.login_button} ${className}`} >
+      className={`login_button`} >
       {text}
       {icon}
     </Button>
