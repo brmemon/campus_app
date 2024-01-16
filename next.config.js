8f2b69fcfc9cf914722ba5d1079f02d391a5eee8
Bilal Raza <126255680+brmemon@users.noreply.github.com>
2024-01-16 20:33:01 +0500

    build clear and schema setting login and signup page vercel errors and next config update and module install

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
+// module.exports = nextConfig
+
+const nextConfig = {
+    // Put other setting here
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            Object.assign(config.resolve.alias, {
+                'react': 'preact/compat',
+                'react-dom/test-utils': 'preact/test-utils',
+                'react-dom': 'preact/compat',
+            })
+        }
+        return config
+    },
+}
 module.exports = nextConfig
