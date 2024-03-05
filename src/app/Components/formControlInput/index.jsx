aa77154db42666c6f5d76398d49c5e14bb8a3760
unknown <bilal raza>
2024-03-06 00:40:57 +0500

    profile update

--- src/app/Components/formControlInput/index.jsx
+++ src/app/Components/formControlInput/index.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react'
-import Input from '../Input';
-import { IoEyeOutline } from 'react-icons/io5';
-import { FaRegEyeSlash } from 'react-icons/fa6';
+import React, { useState } from "react";
+import Input from "../Input";
+import { IoEyeOutline } from "react-icons/io5";
+import { FaRegEyeSlash } from "react-icons/fa6";
 
-const FormControlInput = ({ label, onChange, value, name, id }) => {
-    const [showPassword, setShowPassword] = useState(false);
+const FormControlInput = ({ label, onChange, value, name, id, className }) => {
+  const [showPassword, setShowPassword] = useState(false);
 
-    const PasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
+  const PasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-    return (
-            <div className="password-input-container">
-                <Input
-                    value={value}
-                    name={name}
-                    onChange={onChange}
-                    className="input_eye"
-                    type={showPassword ? 'text' : 'password'}
-                    label={label}
-                    id={id}
-                />
-                <div className="eye-icon" onClick={PasswordVisibility}>
-                    {showPassword ? <IoEyeOutline /> : <FaRegEyeSlash />}
-                </div>
-            </div>
-    )
-}
+  return (
+    <div className="password-input-container">
+      <Input
+        value={value}
+        name={name}
+        onChange={onChange}
+        className={`input_eye ${className}`}
+        type={showPassword ? "text" : "password"}
+        label={label}
+        id={id}
+      />
+      <div className="eye-icon" onClick={PasswordVisibility}>
+        {showPassword ? <IoEyeOutline /> : <FaRegEyeSlash />}
+      </div>
+    </div>
+  );
+};
 
-export default FormControlInput
+export default FormControlInput;
